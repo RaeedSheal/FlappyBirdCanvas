2e3d9c8f4b1feb47f6e4516b40027dc30f7475e7
Raeed <91532317+RaeedSheal@users.noreply.github.com>
2023-06-01 12:30:46 +0300

    fix

--- src/app.js
+++ src/app.js
@@ -1,27 +1,26 @@
-
-let GameArea = {
-  canvas: document.createElement("canvas"),
-  start: function () {
-    this.canvas.width = 480;
-    this.canvas.height = 800;
-    this.canvas.style.cursor = "none";
-    this.canvas.style.border = "1px solid #000000";
-    this.canvas.style.backgroundImage = "url('images/bg-image.jpeg')";
-    this.canvas.style.margin = "auto";
-    this.canvas.style.display = "block";
-    this.context = this.canvas.getContext("2d");
-    document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-    this.interval = setInterval(updateGameArea, 20);
-     clear: function () {
-        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    },
-  },
-};
-
 function startGame() {
     GameArea.start();
     Bird = new component(20, 20, "green", 330, 30);
 }
+
+let GameArea = {
+    canvas: document.createElement("canvas"),
+    start: function () {
+        this.canvas.width = 480;
+        this.canvas.height = 800;
+        this.canvas.style.cursor = "none";
+        this.canvas.style.border = "1px solid #000000";
+        this.canvas.style.backgroundImage = "url('images/bg-image.jpeg')";
+        this.canvas.style.margin = "auto";
+        this.canvas.style.display = "block";
+        this.context = this.canvas.getContext("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        this.interval = setInterval(updateGameArea, 20);
+    },
+    clear: function () {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    },
+};
 
 function component(width, height, color, x, y) {
     this.width = width;
@@ -49,4 +48,3 @@ function updateGameArea() {
 function Jump() {
     Bird.y -= 15;
 }
-
